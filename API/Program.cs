596b08880d109f2469b5ea3626cdc310a4d41c96
Anusha Kotagiri <anusha.kotagiri.anu@gmail.com>
2024-07-26 18:01:45 +0530

    Added angular client

--- API/Program.cs
+++ API/Program.cs
@@ -13,9 +13,9 @@ builder.Services.AddDbContext<DataContext>(opt => {
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 // builder.Services.AddEndpointsApiExplorer();
 // builder.Services.AddSwaggerGen();
-
+builder.Services.AddCors();
 var app = builder.Build();
-
+app.UseCors(x=> x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
 // Configure the HTTP request pipeline.
 // if (app.Environment.IsDevelopment())
 // {
